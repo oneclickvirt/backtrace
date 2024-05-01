0af3ace4abcb0d177e77d8f64b1276fe54196ed1
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-01 09:34:37 +0000

    update

--- backtrace_install.sh
+++ backtrace_install.sh
@@ -2,11 +2,9 @@
 
 arch=$(uname -m)
 if [ "$arch" = "x86_64" ]; then
-  wget -q -O backtrace.tar.gz  https://github.com/oneclickvirt/backtrace/releases/output/backtrace-linux-amd64
-  mv backtrace-linux-amd64 backtrace
+  wget -q -O backtrace  https://github.com/oneclickvirt/backtrace/releases/output/backtrace-linux-amd64
 else
-  wget -q -O backtrace.tar.gz  https://github.com/oneclickvirt/backtrace/releases/output/backtrace-linux-arm64
-  mv backtrace-linux-arm64 backtrace
+  wget -q -O backtrace  https://github.com/oneclickvirt/backtrace/releases/output/backtrace-linux-arm64
 fi
 mv backtrace /usr/bin/
 backtrace
