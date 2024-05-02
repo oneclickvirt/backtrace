96d93915f749eb5c992fb360393733b55a117df7
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-05-02 11:19:00 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,4 +13,6 @@ func main() {
 	}()
 	fmt.Println(Green("项目地址:"), Yellow("https://github.com/oneclickvirt/backtrace"))
 	backtrace.BackTrace()
+	fmt.Println(Purple("同一目标地址显示多个线路时可能追踪IP地址已越过汇聚层，此时除去第一个线路信息，后续信息可能无效"))
+	fmt.Println(Purple("准确线路请查看详细的路由自行判断"))
 }
